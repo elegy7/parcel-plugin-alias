e771fbf0ca40896222f8b19f17186512ea0e0a2a
肖桓炫 <xiaohuanxuan@surveyplus.cn>
2019-05-31 15:11:12 +0800

    fix win10

--- index.js
+++ index.js
@@ -1,65 +1,74 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-module.exports = (bundler) => {
-    let options;
+module.exports = bundler => {
+    let options
 
     try {
-        options = require(path.resolve(__dirname, '../../../.aliasrc'));
-    } catch(e) {
+        options = require(path.resolve(__dirname, '../../../.aliasrc'))
+    } catch (e) {
         // no handler
     }
 
     if (options) {
-        let originResolver = bundler.resolver;
-        let originResolve = originResolver.resolve;
+        let originResolver = bundler.resolver
+        let originResolve = originResolver.resolve
 
         originResolver.resolve = (filename, parent) => {
             if (filename) {
-                for (let name in options) {
-                    let alias = options[name];
+                const result = Object.keys(options).find(key => filename.indexOf(key) !== -1)
+                if (!result) {
+                    filename = filename
+                } else {
+                    // console.log('result', result, filename, parent)
+                    for (let name in options) {
+                        let alias = options[name]
 
-                    if (filename === name || startsWith(filename, name + '/')) {
-                        if (filename !== alias && !startsWith(filename, alias + '/')) {
-                            /**
-                             * compatible with new version(1.7.0)
-                             * @type {boolean}
-                             */
-                            let newVersion = !!originResolver.packageCache;
+                        if (filename === name || startsWith(filename, name + '/')) {
+                            if (filename !== alias && !startsWith(filename, alias + '/')) {
+                                /**
+                                 * compatible with new version(1.7.0)
+                                 * @type {boolean}
+                                 */
+                                let newVersion = !!originResolver.packageCache
 
-                            if (newVersion) {
-                                // filename = alias.replace(bundler.options.rootDir, '') + filename.substr(name.length);
-                                filename = alias + filename.substr(name.length);
-                            } else {
-                                filename = alias + filename.substr(name.length);
-                            }
-                            if (parent) {
-                                // 通过绝对路径计算相对路径以兼容linux
-                                filename = path.relative(parent.substr(0, parent.lastIndexOf('/')), filename)
+                                if (newVersion) {
+                                    // filename = alias.replace(bundler.options.rootDir, '') + filename.substr(name.length);
+                                    filename = alias + filename.substr(name.length)
+                                } else {
+                                    filename = alias + filename.substr(name.length)
+                                }
+                                if (parent) {
+                                    parent = parent.replace(/\\/g, '/')
+                                    // 通过绝对路径计算相对路径以兼容linux
+                                    filename = path
+                                        .relative(parent.substr(0, parent.lastIndexOf('/')), filename)
+                                        .replace(/\\/g, '/')
+                                }
                             }
                         }
                     }
                 }
             }
 
-            return originResolve.call(originResolver, filename, parent);
+            return originResolve.call(originResolver, filename, parent)
         }
     }
 }
 
 function startsWith(string, searchString) {
-    const stringLength = string.length;
-    const searchLength = searchString.length;
+    const stringLength = string.length
+    const searchLength = searchString.length
 
     // early out if the search length is greater than the search string
-    if(searchLength > stringLength) {
-        return false;
+    if (searchLength > stringLength) {
+        return false
     }
-    let index = -1;
-    while(++index < searchLength) {
-        if(string.charCodeAt(index) !== searchString.charCodeAt(index)) {
-            return false;
+    let index = -1
+    while (++index < searchLength) {
+        if (string.charCodeAt(index) !== searchString.charCodeAt(index)) {
+            return false
         }
     }
-    return true;
+    return true
 }
